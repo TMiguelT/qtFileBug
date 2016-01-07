4d1c5756c94b47a8322ddf01b5294349809f7052
Michael Milton <ttmigueltt@gmail.com>
2016-01-07 20:30:56 +0100

    Removed junk

--- CMakeFiles/QtTest_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "QtTest_automoc.cpp"
-  "CMakeFiles/QtTest_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QtTest_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
