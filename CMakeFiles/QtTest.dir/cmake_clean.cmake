4c0c7efa3cc32fd723eea6ed1976ec0762316b52
Michael Milton <ttmigueltt@gmail.com>
2016-01-07 20:27:15 +0100

    Finished

---
+++ CMakeFiles/QtTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "QtTest_automoc.cpp"
+  "CMakeFiles/QtTest.dir/main.cpp.o"
+  "CMakeFiles/QtTest.dir/QtTest_automoc.cpp.o"
+  "QtTest.pdb"
+  "QtTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QDir>
 #include <QUrl>
 #include <QDesktopServices>
+#include <QDebug>
 
 using namespace std;
 
@@ -10,24 +11,33 @@ int main() {
 
     //Create a QFile without a parent, open it and close it, then delete it.
     //Windows refuses to open the file because the file handle hasn't been freed
-    QFile *orphan = new QFile(QDir::temp().path() + "/withoutParent.txt");
+    QString fileName = QDir::tempPath() + "/withoutParent.txt";
+    qDebug() << fileName;
+    QFile *orphan = new QFile(fileName);
     orphan->open(QIODevice::WriteOnly);
     orphan->write("Written something");
     orphan->close();
     delete orphan;
-    QDesktopServices::openUrl(QUrl(orphan->fileName()));
+    bool result = QDesktopServices::openUrl(QUrl::fromLocalFile(fileName));
+    qDebug() << "Succeeded? " << result;
+    qDebug() << QUrl::fromLocalFile(fileName);
 
     getchar();
 
     //Create a QFile with a parent, open it and close it, then delete it.
     //Windows opens the file because the file handle was in the parent, which has been deleted
+    fileName = QDir::tempPath() + "/withParent.txt";
+    qDebug() << fileName;
+
     QObject *parent = new QFile;
-    QFile *child = new QFile(QDir::temp().path() + "/withParent.txt", parent);
+    QFile *child = new QFile(fileName, parent);
     child->open(QIODevice::WriteOnly);
-    orphan->write("Written something else");
+    child->write("Written something else");
     child->close();
     delete parent;
-    QDesktopServices::openUrl(QUrl(orphan->fileName()));
+    QDesktopServices::openUrl(QUrl::fromLocalFile(fileName));
+    qDebug() << "Succeeded? " << result;
+    qDebug() << QUrl::fromLocalFile(fileName);
 
     getchar();
 }
